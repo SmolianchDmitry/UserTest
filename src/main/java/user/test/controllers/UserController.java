23020ba0959b4e36d297c46506fb703446d534d3
Dmitry <7897044@bk.ru>
2022-05-17 10:57:46 +0300

    1.1

--- src/main/java/user/test/controllers/UserController.java
+++ src/main/java/user/test/controllers/UserController.java
@@ -8,9 +8,23 @@ import user.test.Entities.User;
 @RequestMapping("/user")
 public class UserController {
 
-    @SneakyThrows
     @GetMapping("/{userName}")
-    public User getUserByName(@PathVariable final String userName){
+    public User getUserByName(@PathVariable final String userName) {
             return new User(1, "Ivan", "Ivan", "Ivanov", "111@mail.ru", "111", "+375291111111", 1);
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody final User user) {
+
+    }
+
+    @PutMapping("/{userName}")
+    public User  updateUser(@PathVariable final String username, @RequestBody final User user) {
+        return user;
+    }
+
+    @DeleteMapping
+    public void deleteUser(@PathVariable final String userName) {
+        
     }
 }
